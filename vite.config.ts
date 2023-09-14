488969465e6a83cabc085cb1851b72357b998541
repmacrypn <artik3267@gmail.com>
2023-09-14 22:44:31 +0300

    fix: package.json upd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vkGenerator/',
+  base: '/vkLoveConfessa/',
   plugins: [react()],
   resolve: {
     alias: {
